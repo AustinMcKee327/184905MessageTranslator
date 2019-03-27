f08fe010ee099cfa4ff715137d4e6c45db0c777a
AustinMcKee327 <47399911+AustinMcKee327@users.noreply.github.com>
2019-03-27 13:53:59 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _184905MessageTranslator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TranslateButton_Click(object sender, RoutedEventArgs e)
+        {
+            string TextInput = TextBoxShortForm.Text;
+            int End = 0;
+            while (End == 0)
+            {
+                for (int i = 0; i < 12; i++)
+                {
+                    Console.WriteLine("value of i: (0)", i);
+                }
+                TextInput = TextBoxShortForm.Text.ToUpper();
+
+                TextInput = TextInput.Replace(":-)", "I'm Happy" + Environment.NewLine);
+                TextInput = TextInput.Replace(":-(", "I'm Sad" + Environment.NewLine);
+                TextInput = TextInput.Replace(";-)", "Wink" + Environment.NewLine);
+                TextInput = TextInput.Replace(":-P", "Stick Out My Tounge" + Environment.NewLine);
+                TextInput = TextInput.Replace("CU", "See You" + Environment.NewLine);
+                TextInput = TextInput.Replace("~.~", "I'm Sleepy" + Environment.NewLine);
+                TextInput = TextInput.Replace("TA", "Totally Awesome" + Environment.NewLine);
+                TextInput = TextInput.Replace("CCC", "Canadian Computing Competition" + Environment.NewLine);
+                TextInput = TextInput.Replace("CUZ", "Because" + Environment.NewLine);
+                TextInput = TextInput.Replace("TTYL", "Talk To You Later" +Environment.NewLine);
+                TextInput = TextInput.Replace("TY", "Thank You" + Environment.NewLine);
+                TextInput = TextInput.Replace("YW", "Your Welcome" + Environment.NewLine);
+            }
+                                          
+           
+        }
+    }
+}
